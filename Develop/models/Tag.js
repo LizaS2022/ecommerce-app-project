8fd0653676b354c0fc5cd5df3bb437c75bfe519d
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-05-10 09:50:02 -0400

    Initial commit

---
+++ Develop/models/Tag.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    id: {
+      type: DataType.INTEGER,
+      AllowNull: false,
+      primarykey: true,
+      autoIncrement: true,
+    },
+    tag_names: {
+      type: DataType.STRING,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
+
+
